6f1cf965b707737f5f570a311245995ca0369154
Zayd Patel <zayd62@gmail.com>
2019-02-20 22:02:52 +0000

    created information.js for the information card

---
+++ src/Components/Article/Information.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default class extends React.Component {
+    render(){
+        return
+        // tab on render props on react website
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './Components/App'
 
-// this will render app.js
 render(<App />, document.getElementById("root"))
