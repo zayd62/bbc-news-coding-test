e84dfe4363b25eaa97ee601365432b6eaa3afe74
Zayd Patel <zayd62@gmail.com>
2019-02-21 11:54:22 +0000

    Created infomation card
    The information card will display information related to what the web app does.
    
    it also discusses where the articles are obtained from

--- src/Components/Article/Information.js
+++ src/Components/Article/Information.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { Card, CardContent, Typography, } from '@material-ui/core'
+import GridLayout from '../Layouts/GridLayout'
 
-export default class extends React.Component {
-    render(){
-        return
-        // tab on render props on react website
+const style = {
+    card: { padding: 5, marginTop: 10, marginBottom: 5 }
+}
+
+class Intro extends Component {
+    render() {
+        return <Fragment>
+            <Card raised={true} style={style.card}>
+                <CardContent>
+                    <Typography variant="h6" color="inherit">
+                        Introduction
+                    </Typography>
+                    <Typography variant="body2">
+                        This application will show you 3 random articles and will ask you to rate them out of 10
+                        using the dropdown below the article. The articles are automatically fetched from this GitHub Repository.  
+                    </Typography>
+                    <br/>
+                    <Typography variant="body2">
+                        The articles are automatically fetched 
+                        from <a target="_blank" rel="noopener noreferrer" href="https://github.com/bbc/news-coding-test-dataset">this</a> GitHub 
+                        Repository as JSON files. If the fetching fails,
+                        copies of the articles that are stored locally will be used.   
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Fragment>
+    }
+}
+export default class extends Component {
+    render() {
+        return <GridLayout comp={<Intro />} > </GridLayout>
     }
 }
