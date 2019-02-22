025cf00a9fab5f31272c4fe8af8bfdde4ee844d9
Zayd Patel <zayd62@gmail.com>
2019-02-22 00:08:54 +0000

    optimised imports to improve Google Lighthouse performance

--- src/Components/Layouts/GridLayout.js
+++ src/Components/Layouts/GridLayout.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Grid } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
 
 export default class extends Component {
     render() {
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-import { AppBar, Toolbar, Typography} from '@material-ui/core/'
+// import { AppBar, Toolbar, Typography} from '@material-ui/core/'
 
 export default props =>
     <AppBar position="static">
