40f5bf49442ae3ed0a799f6f9a2d40e5234ff9ce
Zayd Patel <zayd62@gmail.com>
2019-02-24 12:46:45 +0000

    created render heading method

--- src/Components/Article/ArticleBody.js
+++ src/Components/Article/ArticleBody.js
@@ -1,12 +1,35 @@
 import React, { Component, Fragment } from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
-class ArticleBody extends Component{
-    render(){
+const style = {
+    card: { padding: 5, marginTop: 10, marginBottom: 5 }
+}
+class ArticleBody extends Component {
+
+    renderHeading(heading, key){
+        return <Typography key={key} variant="h6" color="inherit">
+            {heading}
+        </Typography>
+
+    }
+
+    
+
+    renderArticle(){
+        let header = [];
+        for (let i = 0; i < 3; i++) {
+            header.push(this.renderHeading("blash" + i, i))
+        }
+        return header
+    }
+
+    render() {
         return <Fragment>
-            <h1>This is articleBody</h1>
-            {console.log("hello from article body")}
-            {console.log(this.props.data)}
+            {this.renderArticle()}
         </Fragment>
+
     }
 
 
