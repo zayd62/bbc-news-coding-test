d364d6bb5d4d8a5a4ed4180ce42626ccfa44625f
Zayd Patel <zayd62@gmail.com>
2019-02-24 15:49:16 +0000

    function to generate unrdered lists

--- src/Components/Article/ArticleBody.js
+++ src/Components/Article/ArticleBody.js
@@ -10,41 +10,54 @@ const style = {
 }
 class ArticleBody extends Component {
 
-    generateHeading(heading, key){
+    generateHeading(heading, key) {
         return <Typography key={key} variant="h6" color="inherit">
             {heading}
         </Typography>
 
     }
 
-    generateParagraph(paragraph, key){
-        return <Typography key = {key} 
-        variant="body2"
-        style={{
-            marginBottom:"10px"
-        }}>
+    generateParagraph(paragraph, key) {
+        return <Typography key={key}
+            variant="body2"
+            style={{
+                marginBottom: "10px"
+            }}>
             {paragraph}
-        </Typography> 
+        </Typography>
     }
 
-    generateImage(url, altText, height, width, key){
+    generateImage(url, altText, height, width, key) {
         return <CardMedia
-        component="img"
-        alt={altText}
-        // height="auto"
-        image={url}
-        key={key}
-        style={{
-            maxWidth: width,
-            maxHeight: height, 
-            display : "block", 
-            marginLeft: "auto", 
-            marginRight: "auto",
-            marginTop: "10px", 
-            marginBottom: "10px"}}></CardMedia>
+            component="img"
+            alt={altText}
+            // height="auto"
+            image={url}
+            key={key}
+            style={{
+                maxWidth: width,
+                maxHeight: height,
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                marginTop: "10px",
+                marginBottom: "10px"
+            }}></CardMedia>
     }
 
-    generateArticle(){
+    generateUnorderedList(listitems, key) {
+        for (let i = 0; i < listitems.length; i++) {
+            listitems[i] = <li key={"li" + i}> {listitems[i]} </li>
+        }
+
+        return <Typography key={key} varient="body2">
+            <ul key={key} style={{ fontWeight: "500" }}>
+                {listitems}
+            </ul>
+        </Typography>
+    }
+
+    generateArticle() {
         let article = [];
         // for (let i = 0; i < 3; i++) {
         //     article.push(this.generateHeading("blash" + i, i))
@@ -52,8 +65,9 @@ class ArticleBody extends Component {
         const para = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum laoreet felis vitae convallis. Ut eu ante finibus, blandit neque in, congue enim. Proin ut quam libero. Fusce accumsan lacus eget convallis condimentum. Vestibulum eu ullamcorper justo. Mauris vel bibendum nisi. Vestibulum et tincidunt libero. Vestibulum rhoncus vestibulum sapien vel vehicula. Aliquam erat volutpat. Duis rutrum lectus velit, eget mattis lectus accumsan eu. Nam nisl lectus, tempor vel ullamcorper nec, dignissim at ante.";
         article.push(this.generateHeading("this is the heading", 1))
         article.push(this.generateParagraph(para, 2))
-        article.push(this.generateImage("https://picsum.photos/640/420/?random", "Vestibulum pellentesque laoreet urna, eget dignissim lorem maximus vel", 420, 640,3))
+        article.push(this.generateImage("https://picsum.photos/640/420/?random", "Vestibulum pellentesque laoreet urna, eget dignissim lorem maximus vel", 420, 640, 3))
         article.push(this.generateParagraph(para, 4))
+        article.push(this.generateUnorderedList(["item1", "item2", "item3"], 5))
         return article
     }
 
@@ -62,13 +76,13 @@ class ArticleBody extends Component {
             <GridLayout comp={
                 <Card raised={true} style={style.card}>
                     <CardContent>
-                    {this.generateArticle()}
+                        {this.generateArticle()}
                     </CardContent>
                 </Card>
             }>
 
             </GridLayout>
- 
+
         </Fragment>
 
     }
